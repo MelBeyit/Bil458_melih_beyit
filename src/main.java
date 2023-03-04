93e1928c6139ca2b802262d184489ff94aa31d9a
MelBeyit <melihbeyit2@hotmail.com>
2023-03-04 15:50:59 +0300

    melih beyit

---
+++ src/main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class main {
+
+    public static void main(String[] args){
+
+        float a,b;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter 2 floating number:");
+        a = scan.nextFloat();
+        b = scan.nextFloat();
+        System.out.println("Result is: " + a*b);
+
+
+
+    }
+
+}
